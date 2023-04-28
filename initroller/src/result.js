78e01c33a5836330d55ae50d151420a1a185afa3
Albin Söderholm <albin.johso@gmail.com>
2023-04-29 01:02:13 +0200

    Almost working count

--- initroller/src/result.js
+++ initroller/src/result.js
@@ -5,15 +5,48 @@ import { useEffect } from 'react';
 
 const Result = ({ result }) => {
 
+    const nameMap = {
+        0: "blank",
+        1: "success",
+        2: "advantage",
+        3: "triumph",
+    };
+
+    const count = (outcome1, outcome2) => {
+        let count = new Map();
+        outcome1 = outcome1?.flat();
+        outcome2 = outcome2?.flat();
+        console.log(outcome1);
+        console.log(outcome2);
+        outcome1?.forEach((v) => {
+            if (count.has(nameMap[v])) {
+                count.set(nameMap[v], count.get(nameMap[v]) + 1);
+            } else {
+                count.set(nameMap[v], 1);
+            }
+        });
+        outcome2?.forEach((v) => {
+            if (count.has(nameMap[v])) {
+                count.set(nameMap[v], count.get(nameMap[v]) + 1);
+            } else {
+                count.set(nameMap[v], 1);
+            }
+        });
+        console.log(count);
+        return count;
+    };
+
     useEffect(() => {
-        console.log(result);
+        console.log("Resyt: " + JSON.stringify(result));
     }, [result]);
 
     return (
         <div>  
-            <p>{result.name}</p>
-            <p>{result.skill.join(",")}</p>
-            <p>{result.proficiency.join(",")}</p>
+            <p>Picker name: {result.name}</p>
+            
+                {Array.of(count(result.proficiency, result.skill).entries()).forEach((k, v) => {
+                    <p>{k}: {v}</p>
+                })}
         </div>
     )
 };
--- initroller/src/roller.js
+++ initroller/src/roller.js
@@ -13,29 +13,39 @@ const Roller = ({ diePickers }) => {
 
     const [update, setUpdate] = useState(0);
 
+    const blank = 0;
+    const success = 1;
+    const advantage = 2;
+    const triumph = 3;
+
+
+    const skillMap = [blank, success, success, [success, success], advantage, advantage, [success, advantage], [advantage, advantage]];
+    const proficiencyMap = [blank, success, success, [success, success], [success, success], advantage, [success, advantage], [success, advantage], [success, advantage], [advantage, advantage], [advantage, advantage], triumph];
+
+
+    const mapSkillToSwDice = (dieRolls) => {
+        return dieRolls.map(v => skillMap[v]);
+    };
+
+    const mapProfToSwDice = (dieRolls) => {
+        return dieRolls.map(v => proficiencyMap[v]);
+    };
+
     const rollDice = () => {
-        // loop over diepickers and roll a d8 for each skill and a d12 for each proficiency and load results into two arrays, one for skill and one for proficiency
-
-        
-
-
-        // loop over diepickers and roll a d8 for each skill and a d12 for each proficiency and load results into two arrays, one for skill and one for proficiency
         
         Array.from(diePickers.values()).forEach((diePicker) => {
             let skillRolls = [];
             let proficiencyRolls = [];
             for (let i = 0; i < diePicker.skill; i++) {
-                skillRolls.push(Math.floor(Math.random() * 8) + 1);
+                skillRolls.push(Math.floor(Math.random() * 8));
             }
             for (let i = 0; i < diePicker.proficiency; i++) {
-                proficiencyRolls.push(Math.floor(Math.random() * 12) + 1);
+                proficiencyRolls.push(Math.floor(Math.random() * 12));
             }
-            setResults(results.set(diePicker.id, {name: diePicker.name, id: diePicker.id, skill: skillRolls, proficiency: proficiencyRolls, flag: Math.random()}));
+            setResults(results.set(diePicker.id, {name: diePicker.name, id: diePicker.id, skill: mapSkillToSwDice(skillRolls), proficiency: mapProfToSwDice(proficiencyRolls), flag: Math.random()}));
         });
 
         setUpdate(update + 1);
-
-        console.log(results);
         
     }
 
