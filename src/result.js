ee4b5f8d0ec08c5dca9cf4b0ea6babca78f5398a
Albin Söderholm <albin.johso@gmail.com>
2023-04-29 14:43:40 +0200

    Images

--- src/result.js
+++ src/result.js
@@ -3,13 +3,19 @@
 import React from 'react';
 import { useEffect } from 'react';
 
+import blank from './assets/images/blank.png';
+import success from './assets/images/success.png';
+import advantage from './assets/images/advantage.png';
+import triumph from './assets/images/triumph.png';
+
+
 const Result = ({ result }) => {
 
     const nameMap = {
-        0: "blank",
-        1: "success",
-        2: "advantage",
-        3: "triumph",
+        0: blank,
+        1: success,
+        2: advantage,
+        3: triumph
     };
 
     useEffect(() => {
@@ -19,10 +25,13 @@ const Result = ({ result }) => {
 
     return (
         <div>  
-            <p>Picker name: {result.name}</p>
+            <p>{result.name}</p>
             <div>
                 {result.rolls.map((value, index) => (
-                    <p>{nameMap[index]}: {value}</p>
+
+                    // img tag that displays the advantage image
+
+                    <div className='displayDice'><img src={nameMap[index]}></img>: {value}</div>
                 ))}
             </div>
         </div>
